7ea341a2e10766bbb64cc7dff0babadfabbb01c7
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-09-17 19:35:15 +0200

    lib: svrollbar - fix: main page scrolling

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { Svrollbar, Svroller } from 'svrollbar';
 	import type { PageData } from './$types';
 	import * as Alert from '$components/ui/alert';
 	import Listing from './listing.svelte';
@@ -10,20 +11,22 @@
 	} = data;
 </script>
 
-<section class="flex flex-col gap-2 h-fit overflow-y-scroll">
-	{#await listingsPromise}
-		<p>Fetching listings</p>
-	{:then { data: listings, error }}
-		{#if error}
-			<Alert.Root>
-				<Alert.Title class="font-semibold">Error</Alert.Title>
-				<Alert.Description>{error}</Alert.Description>
-			</Alert.Root>
-		{/if}
-		{#if listings}
-			{#each listings as listing}
-				<Listing {listing} />
-			{/each}
-		{/if}
-	{/await}
-</section>
+<Svroller width="100%" height="80%">
+	<section class="flex flex-col gap-2">
+		{#await listingsPromise}
+			<p>Fetching listings</p>
+		{:then { data: listings, error }}
+			{#if error}
+				<Alert.Root>
+					<Alert.Title class="font-semibold">Error</Alert.Title>
+					<Alert.Description>{error}</Alert.Description>
+				</Alert.Root>
+			{/if}
+			{#if listings}
+				{#each listings as listing}
+					<Listing {listing} />
+				{/each}
+			{/if}
+		{/await}
+	</section>
+</Svroller>
