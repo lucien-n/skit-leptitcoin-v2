95f46a95f870ea168a7661f5c754c14e2455ba61
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-09-17 13:43:20 +0200

    pof: search input cooldown

--- src/routes/(app)/+page.server.ts
+++ src/routes/(app)/+page.server.ts
@@ -1,8 +1,10 @@
 import cfetch from '$lib/cfetch';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ fetch }) => {
-	const listingsPromise = cfetch(`/api/listings/feed`, 'GET', fetch);
+export const load: PageServerLoad = async ({ fetch, url: { searchParams } }) => {
+	const query = searchParams.get('q');
+
+	const listingsPromise = cfetch(`/api/listings/feed${query ? '?q=' + query : ''}`, 'GET', fetch);
 
 	return {
 		streamed: {
--- src/routes/(app)/nav-search.svelte
+++ src/routes/(app)/nav-search.svelte
@@ -1,12 +1,31 @@
-<script>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	import Input from '$components/ui/input/input.svelte';
-	import { Loader2Icon, LoaderIcon, Search } from 'lucide-svelte';
+	import { Loader2Icon, Search } from 'lucide-svelte';
+	import type { FormEventHandler } from 'svelte/elements';
 
 	let searching = false;
 
 	const executeSearch = () => {
 		searching = true;
 		setTimeout(() => (searching = false), 2_000);
+	};
+
+	let last_input_time: number = new Date().getTime();
+
+	const handleInput: FormEventHandler<HTMLInputElement> = (event) => {
+		const target = event.target as HTMLInputElement;
+		const value = target.value;
+
+		$page.url.searchParams.set('q', value);
+
+		const now = new Date().getTime();
+		console.log(now - last_input_time);
+		if (!value || value == '' || now - last_input_time < 2_000) last_input_time = now;
+		else goto($page.url);
+
+		console.log($page.url.searchParams);
 	};
 </script>
 
@@ -16,6 +35,8 @@
 		disabled={searching}
 		placeholder={searching ? 'Searching...' : 'Search'}
 		class="w-full"
+		on:input={handleInput}
+		on:focus={() => (last_input_time = new Date().getTime())}
 	/>
 	<button on:click={executeSearch}>
 		{#if searching}
