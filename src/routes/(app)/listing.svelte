3d4d47ad4fd11505db96aada422dc55762af4666
lucien-neuhoff <lucien.neuhoff@protonmail.com>
2023-09-22 14:55:10 +0200

    feat: picture "modal" (dialog) on listing

--- src/routes/(app)/listing.svelte
+++ src/routes/(app)/listing.svelte
@@ -1,30 +1,45 @@
 <script lang="ts">
 	import ConditionBadge from '$components/lpc/condition-badge.svelte';
 	import * as Card from '$components/ui/card';
+	import * as Dialog from '$components/ui/dialog';
 	import { formatCategory, formatDate } from '$lib/helper';
 
 	export let listing: TListing;
+
+	$: isImageNull = listing.image_url.endsWith('null');
 </script>
 
 <Card.Root class="grid grid-cols-5 h-40 md:h-64">
 	<Card.Header class="p-0 col-span-2">
-		<picture class="bg-muted h-full">
-			{#if !listing.image_url.endsWith('null')}
-				<img
-					src={listing.image_url}
-					alt="listing {listing.title}"
-					class="rounded-l-md object-cover h-40 md:h-64 w-full"
-					loading="lazy"
-				/>
-			{:else}
-				<img
-					src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-					alt=""
-					class="rounded-l-md object-cover h-40 md:h-64 w-full"
-					loading="lazy"
-				/>
-			{/if}
-		</picture>
+		{#if !isImageNull}
+			<Dialog.Root>
+				<Dialog.Trigger class="w-full">
+					<img
+						src={listing.image_url}
+						alt="listing {listing.title}"
+						class="rounded-l-md object-cover h-40 md:h-64 w-full"
+						loading="lazy"
+					/>
+				</Dialog.Trigger>
+				<Dialog.Content class="p-0">
+					{#if !listing.image_url.endsWith('null')}
+						<img
+							src={listing.image_url}
+							alt="listing {listing.title}"
+							class="rounded-l-md object-cover w-full"
+							loading="lazy"
+						/>
+					{/if}
+				</Dialog.Content>
+			</Dialog.Root>
+		{:else}
+			<img
+				src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+				alt=""
+				class="rounded-l-md object-cover h-40 md:h-64 w-full"
+				loading="lazy"
+			/>
+		{/if}
 	</Card.Header>
 	<Card.Content class="col-span-3 pl-2 p-3 flex-col flex justify-between h-full">
 		<a class="flex flex-col w-full h-full group hover:cursor-pointer" href="/listing/{listing.uid}">
