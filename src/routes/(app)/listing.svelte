c3cbdc6ff72f00296785a87b953d8018589862bc
lucien-neuhoff <lucien.neuhoff@protonmail.com>
2023-09-22 14:39:01 +0200

    display a placeholder picture when no picture is found

--- src/routes/(app)/listing.svelte
+++ src/routes/(app)/listing.svelte
@@ -8,14 +8,23 @@
 
 <Card.Root class="grid grid-cols-5 h-40 md:h-64">
 	<Card.Header class="p-0 col-span-2">
-		<div class="bg-muted h-full">
-			<img
-				src={listing.image_url ??
-					'https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80'}
-				alt="listing {listing.title}"
-				class="rounded-l-md object-cover h-40 md:h-64 w-full"
-			/>
-		</div>
+		<picture class="bg-muted h-full">
+			{#if !listing.image_url.endsWith('null')}
+				<img
+					src={listing.image_url}
+					alt="listing {listing.title}"
+					class="rounded-l-md object-cover h-40 md:h-64 w-full"
+					loading="lazy"
+				/>
+			{:else}
+				<img
+					src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+					alt=""
+					class="rounded-l-md object-cover h-40 md:h-64 w-full"
+					loading="lazy"
+				/>
+			{/if}
+		</picture>
 	</Card.Header>
 	<Card.Content class="col-span-3 pl-2 p-3 flex-col flex justify-between h-full">
 		<a class="flex flex-col w-full h-full group hover:cursor-pointer" href="/listing/{listing.uid}">
--- todo.md
+++ todo.md
@@ -1,9 +1,9 @@
 # Todo
 
 - [x] Add cooldown to listings search
-- [ ] Bookmarks page
+- [x] Bookmarks page
 - [ ] Test listing creation (with/without image etc...)
-- [ ] Display a nicer picture if no picture is found
+- [x] Display a nicer picture if no picture is found
 - [ ] Display full picture on click
 - [ ] Listing page ui
 - [ ] Profile page ui
