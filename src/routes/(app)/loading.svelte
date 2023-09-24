974809570752578921fbdb009671fbf2dd09e5ad
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-09-24 20:17:00 +0200

    ref: progress bar animation

--- src/routes/(app)/loading.svelte
+++ src/routes/(app)/loading.svelte
@@ -2,4 +2,22 @@
 	import { slide } from 'svelte/transition';
 </script>
 
-<div transition:slide={{ duration: 200 }} class="w-full fixed flex h-2 bg-primary shadow-sm" />
+<div transition:slide={{ duration: 200 }} class="w-full h-2 bg-primary overflow-x-hidden">
+	<div id="indeterminate-progress" class="h-full w-full bg-white rounded-full" />
+</div>
+
+<style>
+	#indeterminate-progress {
+		animation: progress-animation 2s linear infinite;
+		filter: blur(16px);
+	}
+
+	@keyframes progress-animation {
+		0% {
+			transform: translateX(-100%);
+		}
+		100% {
+			transform: translateX(100%);
+		}
+	}
+</style>
