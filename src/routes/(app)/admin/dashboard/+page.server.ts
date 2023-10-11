eea3eed03f311bea736475c53a148edbc80986f2
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-10-11 16:25:41 +0200

    feat: display and validate listings on admin panel

--- src/routes/(app)/admin/dashboard/+page.server.ts
+++ src/routes/(app)/admin/dashboard/+page.server.ts
@@ -1,12 +1,25 @@
+import { parseListings } from '$lib/server/supabase/listings';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ parent }) => {
+export const load: PageServerLoad = async ({ parent, locals: { supabase } }) => {
 	const { session } = await parent();
 	if (!session) return new Response(null, { status: 401 });
 
-	const getListingsWaitingForValidation = async () => {};
+	const getListingsWaitingForValidation = async (limit: number, offset: number) => {
+		const query = supabase
+			.from('listings')
+			.select(
+				'uid, price, title, description, category, condition, image, created_at, author:profiles(author_uid:uid, name)'
+			)
+			.match({ is_validated: false })
+			.range(offset, limit + offset)
+			.order('created_at', { ascending: true });
 
-	return {
-		listings: getListingsWaitingForValidation()
+		const { data, error }: DbResult<typeof query> = await query;
+		if (error) return { listings: [], error };
+
+		return { listings: parseListings(data) };
 	};
+
+	return await getListingsWaitingForValidation(25, 0);
 };
--- src/routes/(app)/admin/dashboard/+page.svelte
+++ src/routes/(app)/admin/dashboard/+page.svelte
@@ -1,23 +1,64 @@
 <script lang="ts">
+	import Button from '$components/ui/button/button.svelte';
 	import * as Table from '$lib/components/ui/table';
+	import { fly } from 'svelte/transition';
+	import type { PageData, PageServerData } from './$types';
+
+	export let data: PageServerData & PageData;
+
+	const { listings, error, supabase } = data;
+
+	let getListings: TListing[] = [];
+	let validatedUids: string[] = [];
+
+	const validateListing = async (uid: string) => {
+		const { data: success } = await supabase.rpc('validate_listing', { listing_uid: uid });
+		if (success) validatedUids.push(uid);
+		getListings = filterListings();
+	};
+
+	const filterListings = () => {
+		return listings?.filter(({ uid }) => !validatedUids.includes(uid)) || [];
+	};
+
+	getListings = filterListings();
 </script>
 
-<Table.Root>
-	<Table.Caption>A list of your recent invoices.</Table.Caption>
-	<Table.Header>
-		<Table.Row>
-			<Table.Head class="w-[100px]">Invoice</Table.Head>
-			<Table.Head>Status</Table.Head>
-			<Table.Head>Method</Table.Head>
-			<Table.Head class="text-right">Amount</Table.Head>
-		</Table.Row>
-	</Table.Header>
-	<Table.Body>
-		<Table.Row>
-			<Table.Cell class="font-medium">INV001</Table.Cell>
-			<Table.Cell>Paid</Table.Cell>
-			<Table.Cell>Credit Card</Table.Cell>
-			<Table.Cell class="text-right">$250.00</Table.Cell>
-		</Table.Row>
-	</Table.Body>
-</Table.Root>
+{#if error || !listings}
+	<h1 class="text-xl">{error}</h1>
+{:else}
+	<Table.Root>
+		<Table.Caption>Listings in wait of validation.</Table.Caption>
+		<Table.Header>
+			<Table.Row>
+				<Table.Head>Author</Table.Head>
+				<Table.Head>Title</Table.Head>
+				<Table.Head>Description</Table.Head>
+				<Table.Head>Price</Table.Head>
+				<Table.Head>Category</Table.Head>
+				<Table.Head>Condition</Table.Head>
+				<Table.Head>Action</Table.Head>
+			</Table.Row>
+		</Table.Header>
+		<Table.Body>
+			{#each getListings as listing}
+				<Table.Row>
+					<Table.Cell class="font-medium">
+						<Button variant="link" href="/profile/{listing.author.name}">
+							{listing.author.name}
+						</Button>
+					</Table.Cell>
+					<Table.Cell class="font-medium">{listing.title}</Table.Cell>
+					<Table.Cell class="font-medium">{listing.description}</Table.Cell>
+					<Table.Cell class="font-medium">{listing.price}</Table.Cell>
+					<Table.Cell class="font-medium">{listing.category}</Table.Cell>
+					<Table.Cell class="font-medium">{listing.condition}</Table.Cell>
+					<Table.Cell class="font-medium">
+						<Button variant="default" on:click={() => validateListing(listing.uid)}>Validate</Button
+						>
+					</Table.Cell>
+				</Table.Row>
+			{/each}
+		</Table.Body>
+	</Table.Root>
+{/if}
