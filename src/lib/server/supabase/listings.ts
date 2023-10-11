f127df498582323611511affc6c098bf201c152d
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-10-11 14:47:35 +0200

    ref: cleaned up listings api endpoint

---
+++ src/lib/server/supabase/listings.ts
@@ -0,0 +1,61 @@
+export function isListing(obj: unknown): obj is TListing {
+	if (typeof obj === 'object' && obj !== null) {
+		const authorInObj = 'author' in obj && obj.author !== null && typeof obj.author === 'object';
+		if (!authorInObj) return false;
+
+		const author: object = obj.author as object;
+
+		return (
+			'uid' in obj &&
+			'uid' in author &&
+			'name' in author &&
+			'price' in obj &&
+			'title' in obj &&
+			'description' in obj &&
+			'category' in obj &&
+			'condition' in obj &&
+			'image' in obj &&
+			'created_at' in obj
+		);
+	}
+	return false;
+}
+
+export const parseListing = (data: unknown): TListing | null => {
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	const authorData: { author_uid: string; name: string } = data.author;
+
+	if (!isListing(data)) return null;
+
+	const listing = {
+		uid: data.uid,
+		author: {
+			uid: authorData.author_uid,
+			name: authorData.name
+		},
+		price: data.price,
+		title: data.title,
+		description: data.description,
+		category: data.category,
+		condition: data.condition,
+		created_at: data.created_at,
+		image: data.image
+	} satisfies TListing;
+
+	return listing;
+};
+
+export const parseListings = (data: unknown[] | undefined): TListing[] => {
+	console.log(data?.length);
+	if (!data || data.length === 0) return [];
+
+	const listings: TListing[] = [];
+	for (const listingData of data) {
+		const parsed = parseListing(listingData);
+		if (parsed) listings.push(parsed);
+	}
+	console.log(listings.length);
+
+	return listings;
+};
--- src/routes/(api)/api/listings/[uid]/+server.ts
+++ src/routes/(api)/api/listings/[uid]/+server.ts
@@ -1,26 +1,11 @@
-import { getHeaders, getRouteExpiration } from '$lib/server/cache';
+import { getRouteExpiration } from '$lib/server/cache';
 import { checkUid } from '$lib/server/helper';
 import { redis } from '$lib/server/redis';
+import { parseListings } from '$lib/server/supabase/listings';
+import type { SupabaseClient } from '@supabase/supabase-js';
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async ({
-	setHeaders,
-	locals: { supabase },
-	params: { uid }
-}) => {
-	if (!uid) return new Response(null, { status: 422 });
-
-	const headers = getHeaders('listings/listing');
-
-	let uids: string[] = [];
-
-	if (uid.length > 36) uids = uid.split(';');
-	else uids.push(uid);
-	uids = uids.filter((id) => checkUid(id));
-
-	const offset = 0;
-	const limit = 10;
-
+const getCachedBookmarkedListings = async (uids: string[]) => {
 	const listings: TListing[] = [];
 
 	for (const id of uids) {
@@ -33,47 +18,65 @@ export const GET: RequestHandler = async ({
 		}
 	}
 
-	if (uids.length == 0) return new Response(JSON.stringify({ data: listings }), { status: 200 });
+	return { listings, uids };
+};
 
+const getSupaBookmarkedListings = async (
+	supabase: SupabaseClient,
+	uids: string[],
+	offset: number = 0,
+	limit: number = 10
+) => {
 	const query = supabase
 		.from('listings')
 		.select(
-			'uid, price, title, description, category, condition, image, created_at, author:profiles(author_uid:uid, name)'
+			'uid, price, title, description, category, condition, image, created_at, author:profiles(uid:uid, name)'
 		)
 		.match({ is_validated: true })
 		.in('uid', uids)
 		.range(offset, limit + offset);
 
-	const { data, error }: DbResult<typeof query> = await query;
+	const { data, error, status }: DbResult<typeof query> = await query;
 
-	if (error) return new Response(JSON.stringify({ error }), { status: 500 });
-	if (!(data.length > 0)) return new Response(null, { status: 204 });
+	if (error) return { error, status };
+	if (!(data.length > 0)) return { status };
+	return { data };
+};
 
-	for (const listingData of data) {
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore
-		const authorData: { author_uid: string; name: string } = listingData.author;
-
-		const listing = {
-			uid: listingData.uid,
-			author: {
-				uid: authorData.author_uid,
-				name: authorData.name
-			},
-			price: listingData.price,
-			title: listingData.title,
-			description: listingData.description,
-			category: listingData.category,
-			condition: listingData.condition,
-			created_at: listingData.created_at,
-			image: listingData.image
-		} satisfies TListing;
-
+const cacheSupaListings = async (supaListings: TListing[]) => {
+	for (const listing of supaListings) {
 		const redisKey = 'listing:' + listing.uid;
 		redis.set(redisKey, JSON.stringify(listing), 'EX', getRouteExpiration('listings/listing'));
 	}
+};
 
-	setHeaders(headers);
+export const GET: RequestHandler = async ({ locals: { supabase }, params: { uid } }) => {
+	if (!uid) return new Response(null, { status: 422 });
 
-	return new Response(JSON.stringify({ data: listings }), { status: 200 });
+	let uids: string[] = [];
+
+	if (uid.length > 36) uids = uid.split(';');
+	else uids.push(uid);
+	uids = uids.filter((id) => checkUid(id));
+
+	const offset = 0;
+	const limit = 10;
+
+	const { listings: cachedListings, uids: remainingUids } = await getCachedBookmarkedListings(uids);
+
+	if (remainingUids.length == 0)
+		return new Response(JSON.stringify({ data: cachedListings }), { status: 200 });
+
+	const { data, error, status } = await getSupaBookmarkedListings(
+		supabase,
+		remainingUids,
+		offset,
+		limit
+	);
+	if (error) return new Response(JSON.stringify({ error }), { status });
+	const supaListings = parseListings(data);
+
+	await cacheSupaListings(supaListings);
+
+	return new Response(JSON.stringify({ data: supaListings }), { status: 200 });
 };
